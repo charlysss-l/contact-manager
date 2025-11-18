828866e5ecd8788b28cf3181e26b13acbac35ce8
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2025-11-18 11:12:56 +0800

    CRUD

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -17,4 +17,43 @@ router.post('/', async(req,res)=>{
     }
 });
 
+//Read All
+router.get('/', async(req,res)=>{
+    try {
+        const contacts = await Contact.find();
+        res.json(contacts);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+//Read One
+router.get('/:id', async(req,res)=>{
+    try {
+        const contact = await Contact.findById(req.params.id);
+        if(!contact) return res.status(404).json({ message: 'Contact not found' });
+        res.json(contact);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+//Update
+router.put('/:id', async(req,res)=>{
+    try {
+        const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedContact);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+//Delete
+router.delete('/:id', async (req,res)=>{
+    try {
+        const deletedContact = await Contact.findByIdAndDelete(req.params.id);
+        if (!deletedContact) return res.status(404).json({ message: 'Contact not found' });
+        res.json({ message: 'Contact deleted' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 export default router;
