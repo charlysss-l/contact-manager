cf70cf9c222c9fe3f76c33c96a82067d672f55c1
Charlize Mikaela Nadela <105686280+charlysss-l@users.noreply.github.com>
2025-11-18 15:34:42 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
   
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
